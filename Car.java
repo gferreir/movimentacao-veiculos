89de081cb4a2799ece232378095bbca3c56bab9a
gferreir <guilherme.nmf@gmail.com>
2018-04-18 14:12:34 -0300

    inclusão de dados

--- Car.java
+++ Car.java
@@ -3,7 +3,6 @@ package projeto_poo;
 public class Car extends Vehicles {
 
 	public Car(int x, int y, boolean factory) {
-		//chamada metedo que gera a posição x e y
 		super(x, y, 2, factory, "red");
 		
 	}
